16349372de84978d491c36ed60e161859dd401bf
Tim Flucker <tflucker@gwmail.gwu.edu>
2020-09-13 22:50:21 -0400

    update popup ui, added text color change

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 chrome.runtime.onInstalled.addListener(function(){
-    chrome.storage.sync.set({defaultColor1: '#FFFFFF'}, function(){
-        console.log("The color is white");
-    });
-    chrome.storage.sync.set({defaultColor2: '#000000'}, function(){
-        console.log("The color is white");
-    });
-    
+       
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
--- popup.js
+++ popup.js
@@ -1,50 +1,13 @@
-let changeColor = document.getElementById('changeColor1');
-let changeColor2 = document.getElementById('changeColor2');
+// get backgroundBtnDiv element
+let backgroundBtnDiv = document.getElementById("backgroundBtnDiv");
 
-// default color (white)
-chrome.storage.sync.get('defaultColor1', function(data){
-    changeColor.style.backgroundColor = data.defaultColor1;
-    changeColor.setAttribute('value', data.defaultColor1);
-});
-
-// default color (black)
-chrome.storage.sync.get('defaultColor2', function(data){
-    changeColor2.style.backgroundColor = data.defaultColor2;
-    changeColor2.setAttribute('value', data.defaultColor2);
-});
-
-// onclick for defaultColor1 (white)
-changeColor.onclick = function (element){
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.background = "' + color + '";'});
-    });
-};
-
-// onclick for defaultColor2 (black)
-changeColor2.onclick = function (element){
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.background = "' + color + '";'});
-    });
-};
-
-// get buttonDiv element
-let page = document.getElementById("buttonDiv");
-
-// list of possible colors
-const kButtonColors = ['#3aa757','#e8453c','#f9bb2d','#4688f1'];
+// list of possible colors for background color
+const kButtonColors = ['#FFFFFF','#000000','#3aa757','#e8453c','#f9bb2d','#4688f1'];
 
 // create buttons using those colors
 function constructOptions(kButtonColors){
-    console.log("options: "+kButtonColors);
     for(var i=0; i<kButtonColors.length; i++){
         var item = kButtonColors[i];
-        console.log("item: " + item);
         // create button
         let button = document.createElement("button");
         // set background color and value to item hexcode
@@ -62,9 +25,44 @@ function constructOptions(kButtonColors){
             });
         });
         // add button to 'buttonDiv' div
-        page.appendChild(button);
+        backgroundBtnDiv.appendChild(button);
+    }
+}
+
+
+// get backgroundBtnDiv element
+let textBtnDiv = document.getElementById("textBtnDiv");
+
+// list of possible colors for text color
+const textBtnColors = ["#FFFFFF","#000000","#808080"];
+
+// creates buttons for possible text colors
+function createTextColors(textBtnColors){
+    for(var i=0; i<textBtnColors.length; i++){
+        var item = textBtnColors[i];
+        // create button
+        let button = document.createElement("button");
+        // set background color and value to item hexcode
+        button.style.backgroundColor = item;
+        button.setAttribute('value', item);
+        // assign click event
+        button.addEventListener('click', function(element){
+            // get color value (hexcode)
+            let color = element.target.value;
+            // for clicked item change background color of current tab
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {code: 'document.body.style.color = "' + color + '";'});
+            });
+        });
+        // add button to 'buttonDiv' div
+        textBtnDiv.appendChild(button);
     }
 }
 
 // call costructOptions method
 constructOptions(kButtonColors);
+
+// call createTextColors function
+createTextColors(textBtnColors);
